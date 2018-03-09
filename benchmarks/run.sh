b8c8618de24c7fbea16a40be299ecdd245c66d92
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-03-09 11:56:06 +0900

    explicitly convert to hash

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -uex
 
+function git_commit() {
+  git show --format="%H" "$@"
+}
+
 function run_benchmark() {
   CHAINER_COMMIT="${1}"; shift
   CUPY_COMMIT="${1}"; shift
+
+  # Ensure to use commit hash in case CHAINER_COMMIT is a branch.
+  CHAINER_COMMIT="$(git_commit ${CHAINER_COMMIT})"
 
   # Clone CuPy.
   if [ ! -d cupy ]; then
@@ -12,11 +19,16 @@ function run_benchmark() {
   # Checkout the branch to use.
   pushd cupy
   git checkout "${CUPY_COMMIT}"
+
+  # Ensure to use commit hash in case CUPY_COMMIT is a branch.
+  CUPY_COMMIT="$(git_commit ${CUPY_COMMIT})"
   popd
 
   # Run the benchmark.
   # The benchmark environment depends on ${CUPY_COMMIT}.
   export VOLATILE_VIRTUALENV_KEY="${CUPY_COMMIT}"
+  export PIP_VERBOSE=True
+  export PIP_LOG=pip.log
   asv run --step 1 "$@" "${CHAINER_COMMIT}"
 }
 
